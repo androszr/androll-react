ad29094b866fbf631b15505326307fc2dc55a38a
androszr <androszr@gmail.com>
2020-08-09 16:56:20 +0200

    add portfolio project box and meta tags

---
+++ src/components/Features/SelectedProject/SelectedProject.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './SelectedProject.scss';
+import Icon from '../../Common/Icon/Icon';
+
+const SelectedProject = () => (
+  <div className={styles.component}>
+    <div className={styles.close}><Icon name='fal fa-times'/></div>
+    <h4 className={styles.projectText + ' ' + styles.projectHeader}>Project name</h4>
+    <p className={styles.projectText + ' ' + styles.projectDescription}>Project description.</p>
+    <div className={styles.projectGrid}>
+      <div className={styles.technology}>
+        <p className={styles.gridText}>Technology:</p>
+        <Icon name='fab fa-html5'/>
+        <Icon name='fab fa-css3-alt'/>
+        <Icon name='fab fa-js'/>
+        
+      </div>
+      <div className={styles.links}>
+        <p className={styles.gridText}>Learn more:</p>
+        <Icon name='fab fa-github'/>
+        <Icon name='fal fa-link'/>
+      </div>
+    </div>
+  </div>
+);
+
+export default SelectedProject;
--- src/components/Views/Portfolio/Portfolio.js
+++ src/components/Views/Portfolio/Portfolio.js
@@ -3,6 +3,7 @@ import styles from './Portfolio.scss';
 import Container from '../../Layout/Container/Container';
 import PageTitle from '../../Common/PageTitle/PageTitle';
 import Map from '../../Features/Map/Map';
+import SelectedProject from '../../Features/SelectedProject/SelectedProject';
 //import PropTypes from 'prop-types';
 //import {settings} from '../../../data/dataStore';
 
@@ -11,6 +12,10 @@ const Portfolio = () => (
     <Container>
       <PageTitle title='Portfolio' />
       <Map />
+      <div className={styles.project}>
+        <SelectedProject />
+      </div>
+      
     </Container>
    
   </div>
